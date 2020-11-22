09bed4c777bffe5b6cc802d891bb09d2411ba5b0
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-11-22 00:04:58 -0500

    filtering component interface

--- movierentals/src/components/movies.jsx
+++ movierentals/src/components/movies.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import Pagination from './common/pagination'
+import Pagination from './common/pagination';
+import ListGroup from './common/listGroup';
 import { getMovies } from "../services/fakeMovieService";
+import {getGenres} from "../services/fakeGenreService";
 import {paginate} from '../utils/paginate';
 
 class Movies extends Component {
   state = {
-    movies: getMovies(),
+    movies: [],
+    genres: [],
     currentPage: 1,
     pageSize: 4
   };
+
+  componentDidMount(){
+    this.setState({ movies: getMovies(), genres: getGenres()});
+  }
 
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -32,6 +39,10 @@ class Movies extends Component {
     this.setState({currentPage: page});
   };
 
+  handleGenreSelect = genre => {
+    console.log(genre);
+  };
+
   render() {
     const { length: count } = this.state.movies;
     const {pageSize, currentPage, movies: allMovies} = this.state;
@@ -39,49 +50,54 @@ class Movies extends Component {
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     const movies = paginate(allMovies, currentPage, pageSize);
-
+ 
     return (
-      <React.Fragment>
-        <p>showing {this.state.movies.length} movies in the database</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>              
-                    <Like liked={movie.liked} onClick={() => this.handleLike(movie) }/>
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination 
-                itemsCount={count} 
-                pageSize={pageSize} 
-                onPageChange={this.handlePageChange}
-                currentPage = {currentPage}
-        />
-      </React.Fragment>
+      <div className = "row">
+          <div className="col-2">
+            <ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect}/> 
+          </div>
+          <div className="col">
+              <p>showing {count} movies in the database</p>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {movies.map((movie) => (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>              
+                          <Like liked={movie.liked} onClick={() => this.handleLike(movie) }/>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => this.handleDelete(movie)}
+                          className="btn-danger btn-sm"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <Pagination 
+                      itemsCount={count} 
+                      pageSize={pageSize} 
+                      onPageChange={this.handlePageChange}
+                      currentPage = {currentPage}
+              />
+          </div>
+      </div>
     );
   }
 }
